244ce9c2bc23ccde92988077229103b7f5377c03
@mabbamOG <mariobarbara@protonmail.ch>
2017-12-12 12:23:36 +0100

    just ignore fucking haskell

--- 0x02/stat.hs
+++ 0x02/stat.hs
@@ -1,12 +1,19 @@
 import Data.Char 
 import qualified Data.Map.Lazy as Map
 import Data.List
-import Control.Exception -- try
+--import Control.Exception
 import Text.Printf
+import System.Environment
+import Control.Monad
+import System.Exit
 
+main :: IO ()
 main = do
     -- OPEN FILE
-    file <- readFile "/tmp/test.txt"
+    argv <- getArgs
+    when (null argv) exitFailure
+    file <- readFile (head argv)
+
 
     -- GENEREATE STRUCTURES
     let d_words  = Map.fromListWith (+) [(word,1) | word<-words (map toLower file)]
@@ -14,8 +21,7 @@ main = do
 
     -- GENEREATE STATISTICS
     let tot = Map.foldl (+) 0 d_lengths
-    --let avg =  (Map.foldlWithKey (\key acc val -> acc+val*key) 0 d_lengths) `quot`  tot
-    let avg =  (Map.foldlWithKey (\key acc val -> acc+val*key) 0 d_lengths) `quot`  tot -- FIX
+    let avg =  (Map.foldlWithKey (\acc key val -> acc+val*key) 0 d_lengths) `quot` tot
     let stat = Map.assocs d_lengths
     let _popular = sortBy (\a b-> (flip compare) (snd a) (snd b)) (Map.assocs d_words)
     let popular = take 10 _popular
